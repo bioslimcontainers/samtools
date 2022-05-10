c3c0fb591b18e2ec0409122e518a2f9662632ba7
Yasunobu Okamura <okamura@informationsea.info>
2022-05-10 23:33:23 +0900

    update build script

--- build.sh
+++ build.sh
@@ -8,4 +8,23 @@ for one_ver in 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15 1.15.1; do
         --build-arg BCFTOOLS_VERSION=${one_ver} \
         --build-arg HTSLIB_VERSION=${one_ver} \
         --tag bioslimcontainers/samtools:${one_ver} .
+
+    docker build \
+        --build-arg SAMTOOLS_VERSION=${one_ver} \
+        --build-arg BCFTOOLS_VERSION=${one_ver} \
+        --build-arg HTSLIB_VERSION=${one_ver} \
+        --tag bioslimcontainers/bcftools:${one_ver} .
+
+    docker build \
+        --build-arg SAMTOOLS_VERSION=${one_ver} \
+        --build-arg BCFTOOLS_VERSION=${one_ver} \
+        --build-arg HTSLIB_VERSION=${one_ver} \
+        --tag bioslimcontainers/htslib:${one_ver} .
+
+    if [ ! -z "${DOCKER_PUSH}" ]; then
+        docker push bioslimcontainers/samtools:${one_ver}
+        docker push bioslimcontainers/bcftools:${one_ver}
+        docker push bioslimcontainers/htslib:${one_ver}
+    fi
+
 done
